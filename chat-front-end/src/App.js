dc160c674b591d08d3f04f7e395a6aa9584d42a1
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-10-04 11:55:22 -0300

    feat: conectar com signalR no back-end

--- chat-front-end/src/App.js
+++ chat-front-end/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Lobby from './components/Lobby';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import { useState } from 'react';
 
 function App() {
+  const [connection, setConnection] = useState();
+
+  const joinRoom = async (user, room) => {
+    try {
+      const connection = new HubConnectionBuilder()
+        .withUrl("https://localhost:7114/chat")
+        .configureLogging(LogLevel.Information)
+        .build();
+
+      connection.on("ReceiveMessage", (user, message) => {
+        console.log('message received: ', message);
+      });
+
+      await connection.start();
+      await connection.invoke("JoinRoom", { user, room });
+
+      setConnection(connection);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>My Chat</h2>
+      <hr className="line" />
+      <Lobby joinRoom={joinRoom} />
     </div>
   );
 }
