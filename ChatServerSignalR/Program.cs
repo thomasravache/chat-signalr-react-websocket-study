4592d93cee075ba1910df0a4686747cafc2de251
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-10-04 11:50:26 -0300

    feat: adicionar politica de cors

--- ChatServerSignalR/Program.cs
+++ ChatServerSignalR/Program.cs
@@ -4,7 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+{
+  options.AddDefaultPolicy(builder =>
+  {
+    builder.WithOrigins("http://localhost:3000")
+      .AllowAnyHeader()
+      .AllowAnyMethod()
+      .AllowCredentials();
+  });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapGet("/", () => "Hello World!");
 
